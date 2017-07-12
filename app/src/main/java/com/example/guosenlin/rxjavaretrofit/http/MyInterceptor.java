5a2f30659ae92eb8faa710512027f3b70052922e
guosenlin <guosenlin91@gmail.com>
2017-07-12 16:49:57 +0800

    Retrofit 添加参数签名demo（Post请求）

--- app/src/main/java/com/example/guosenlin/rxjavaretrofit/http/MyInterceptor.java
+++ app/src/main/java/com/example/guosenlin/rxjavaretrofit/http/MyInterceptor.java
@@ -3,10 +3,16 @@ package com.example.guosenlin.rxjavaretrofit.http;
 import android.util.Log;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
+import okhttp3.FormBody;
 import okhttp3.HttpUrl;
 import okhttp3.Interceptor;
 import okhttp3.Request;
@@ -60,5 +66,45 @@ public class MyInterceptor implements Interceptor {
         return request;
 
 
+    }
+
+
+    //post 添加签名和公共参数
+    private Request addPostParams(Request request) throws UnsupportedEncodingException {
+        if (request.body() instanceof FormBody) {
+            FormBody.Builder bodyBuilder = new FormBody.Builder();
+            FormBody formBody = (FormBody) request.body();
+
+            //把原来的参数添加到新的构造器，（因为没找到直接添加，所以就new新的）
+            for (int i = 0; i < formBody.size(); i++) {
+                bodyBuilder.addEncoded(formBody.encodedName(i), formBody.encodedValue(i));
+            }
+
+            formBody = bodyBuilder
+                    .addEncoded("ver","" )
+                    .addEncoded("time", String.valueOf(System.currentTimeMillis()))
+                    .build();
+
+            Map<String, String> bodyMap = new HashMap<>();
+            List<String> nameList = new ArrayList<>();
+
+            for (int i = 0; i < formBody.size(); i++) {
+                nameList.add(formBody.encodedName(i));
+                bodyMap.put(formBody.encodedName(i), URLDecoder.decode(formBody.encodedValue(i), "UTF-8"));
+            }
+
+
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < nameList.size(); i++) {
+                builder.append("&").append(nameList.get(i)).append("=")
+                        .append(URLDecoder.decode(bodyMap.get(nameList.get(i)), "UTF-8"));
+            }
+
+            formBody = bodyBuilder.
+                    addEncoded("sign", EncryptDecryptTool.stringToMd5(builder.toString()))
+                    .build();
+            request = request.newBuilder().post(formBody).build();
+        }
+        return request;
     }
 }
